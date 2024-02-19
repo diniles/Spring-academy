b54e6518d0fa178f83c6e230c2d7edc8fe4d7bb0
Dzmitry Liashko <dzliashko@gmail.com>
2024-02-20 01:20:12 +0300

    Feat: Create a REST Controller FAILED!!!

---
+++ src/main/java/example/cashcard/CashCardController.java
@@ -0,0 +1,9 @@
+package example.cashcard;
+
+import org.springframework.http.ResponseEntity;
+
+public class CashCardController {
+    private ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
